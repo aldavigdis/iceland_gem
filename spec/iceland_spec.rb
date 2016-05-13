12e22eee03c75ea7e27510ee089d4e86d7e91a70
Stefan Vignir <stefanvignir@stefanvignir.is>
2016-05-14 01:15:04 +0200

    Adding tests for the String class

--- spec/iceland_spec.rb
+++ spec/iceland_spec.rb
@@ -120,3 +120,12 @@ describe Kennitala do
     end
   end
 end
+
+describe String do
+  it 'can be cast to kennitala using .to_kt' do
+    expect('0101302989'.to_kt).to be_an_instance_of(Kennitala)
+  end
+  it 'will not be cast to a kennitala if invalid and will raise an error' do
+    expect { ''.to_kt }.to raise_error(ArgumentError, 'Kennitala is invalid')
+  end
+end
