8da5e5b9e23b34e8aafbe3ba99a2c04fdac2d9fd
Stefan Vignir <stefanvignir@stefanvignir.is>
2016-05-14 00:35:19 +0200

    Adding a kennitala monkey patch for the String class

--- lib/iceland.rb
+++ lib/iceland.rb
@@ -258,3 +258,13 @@ class Kennitala
     false
   end
 end
+
+# Monkey patch for the String class
+class String
+  # Converts a String to a Kennitala object
+  #
+  # @return [Kennitala]
+  def to_kt
+    Kennitala.new(self)
+  end
+end
